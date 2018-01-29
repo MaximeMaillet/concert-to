0c5a6ba11e923a966c0040d1ea2d3f10302814f6
Maxime Maillet <maxime.maillet93@gmail.com>
2018-01-29 01:52:20 +0100

    middleware authorization

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,6 +1,4 @@
-'use strict';
-
-module.exports.run = (req, res, next) => {
+module.exports.use = (req, res, next) => {
   if(!req.session || !req.session.user) {
     return res.sendStatus(401);
   } else {
